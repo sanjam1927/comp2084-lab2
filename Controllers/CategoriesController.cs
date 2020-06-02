c2d3a6ef551b9e2fce54b6d021229f82842e9a3d
sanjam1927 <sanambhatia.sb@gmail.com>
2020-06-01 22:51:51 -0400

    Add project files.

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab2.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details(String Category)
+        {
+            ViewBag.Category = Category;
+            return View();
+        }
+    }
+}
